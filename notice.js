0f40317db3f379bf3f45cad845dfa290dc91b0a4
Istiyaq Ahmad <ahmedistiyaqqq03@gmail.com>
2020-03-25 19:07:59 +0000

    k

---
+++ notice.js
@@ -0,0 +1,3 @@
+var info = document.getElementById("info");
+
+info.innerHTML = "<strong>&copy2020 Pok&eacute;mon / &copy The Pok&eacute;mon Company / &copy The Pok&eacute;mon Company / DuArt, Film and Video, inc</strong>"
