d58f5adca9df7cb945237ce08aee1693ed5f1698
Istiyaq Ahmad <ahmedistiyaqqq03@gmail.com>
2020-03-26 01:14:48 +0000

    k

--- notice.js
+++ notice.js
@@ -1,3 +1,8 @@
 var info = document.getElementById("info");
 
 info.innerHTML = "<strong>&copy2020 Pok&eacute;mon / &copy The Pok&eacute;mon Company / &copy The Pok&eacute;mon Company / DuArt, Film and Video, inc</strong>"
+
+
+if (screen.width <= 699) {
+	location.replace("https://watchpokemontv.netlify.com/");
+}
